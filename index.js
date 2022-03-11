57ebcc9c2e0cb943b57722e9de9f7eba0ad22f3e
Zhekabas <101421541+Zhekabas@users.noreply.github.com>
2022-03-11 20:37:18 +0200

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,10 @@
+    <version number="$Revision: 1.3 $"/>
+    <generation date="$Date: 2009/05/05 23:15:57 $"/>
+	<telephoneCodeData>
+		<codesByTerritory territory="001">
+			<telephoneCountryCode code="991"/>
+		</codesByTerritory>
+
+		<codesByTerritory territory="AC">
+			<telephoneCountryCode code="247"/>
+		</codesByTerritory>
